c42b8529c5f20d39fc270ca6452c04cf4ee44f78
Gabiss1 <136657181+Gabiss1@users.noreply.github.com>
2025-02-26 18:05:46 -0300

    Add files via upload

---
+++ src/Atividades/22-02-25/harryPotter.ts
@@ -0,0 +1,185 @@
+// Varinha
+
+export class Varinha {
+    private madeira:string
+    private nucleo:string
+    private tamanho:number
+
+    setAtributos(madeira:string, nucleo:string, tamanho:number):void{
+        this.madeira = madeira
+        this.nucleo = nucleo
+        this.tamanho = tamanho
+    }
+
+    getMadeira():void{
+        console.log(this.madeira)
+    }
+
+    getNucleo():void{
+        console.log(this.nucleo)
+    }
+
+    getTamanho():void{
+        console.log(this.tamanho)
+    }
+
+    exibirDetalhes():void{
+        console.log(`Esta varinha é feita de madeira de ${this.madeira}, tem um núcleo de ${this.nucleo} e o tamanho de ${this.tamanho} centímetros.`)
+    }
+}
+
+const novaVarinha = new Varinha()
+novaVarinha.setAtributos('Cereijeira', 'Pena de Fênix', 15)
+novaVarinha.exibirDetalhes()
+
+// Chapéu seletor
+
+export class ChapeuSeletor{
+    private casa:string
+
+    selecionarCasa():void{
+        let numero:number = Math.floor(Math.random()*4+1)
+
+        if (numero === 1) {
+            this.casa = 'Grifinória'
+        } else   if (numero === 2) {
+            this.casa = 'Sonserina'
+        } else   if (numero === 3) {
+            this.casa = 'Corvinal'
+        } else   if (numero === 4) {
+            this.casa = 'Lufa-Lufa'
+        } else {
+            console.log('Deu erro')
+        }
+    }
+
+    exibirCasa():void{
+        console.log(`Este aluno é da casa de ${this.casa}!`)
+    }
+}
+
+const novoAluno = new ChapeuSeletor()
+novoAluno.selecionarCasa()
+novoAluno.exibirCasa()
+
+// Feitiços
+
+export class Feiticos{
+    private nome:string
+    private tipo:string
+    private poder:number
+
+    setAtributos(nome:string, tipo:string, poder:number):void{
+        this.nome = nome
+        this.tipo = tipo
+        this.poder = poder
+    }
+
+    getNome():void{
+        console.log(this.nome)
+    }
+
+    getTipo():void{
+        console.log(this.tipo)
+    }
+
+    getPoder():void{
+        console.log(this.poder)
+    }
+
+    exibirFeitico():void{
+        console.log(`
+Nome do Feitiço: ${this.nome}.
+Tipo de Feitiço: ${this.tipo}.
+Poder do Feitiço: ${this.poder}`)
+    }
+
+    lancarFeitico():void{
+
+        if (this.tipo === 'Defesa') {
+            let defesaFeitico = Math.floor(Math.random()*(this.poder-15)+15)
+            console.log(`O feitico ${this.nome} do tipo ${this.tipo} foi lançado! O dano causado a você será reduzido em ${defesaFeitico} pontos.`)
+        } else if (this.tipo === 'Cura') {
+            let curaFeitico = Math.floor(Math.random()*(this.poder-15)+15)
+            console.log(`O feitico ${this.nome} do tipo ${this.tipo} foi lançado! A cura gerada foi de ${curaFeitico} pontos`)
+        } else if (this.tipo === 'Ataque') {
+            let danoFeitico = Math.floor(Math.random()*(this.poder-10)+10)
+            console.log(`O feitico ${this.nome} do tipo ${this.tipo} foi lançado! O dano causado foi de ${danoFeitico} pontos`)
+        }
+        
+    }
+}
+
+const novoFeitico = new Feiticos()
+const novoFeitico1 = new Feiticos()
+const novoFeitico2 = new Feiticos()
+const novoFeitico3 = new Feiticos()
+const novoFeitico4 = new Feiticos()
+
+novoFeitico.setAtributos('Expecto Patronum', 'Defesa', 60)
+novoFeitico1.setAtributos('Expelliarmus', 'Ataque', 25)
+novoFeitico2.setAtributos('Protego', 'Defesa', 30)
+novoFeitico3.setAtributos('Cruciatus', 'Ataque', 45)
+novoFeitico4.setAtributos('Fogo Maldito', 'Ataque', 95)
+
+novoFeitico.lancarFeitico()
+novoFeitico1.lancarFeitico()
+novoFeitico2.lancarFeitico()
+novoFeitico3.lancarFeitico()
+novoFeitico4.lancarFeitico()
+
+novoFeitico.exibirFeitico()
+novoFeitico1.exibirFeitico()
+novoFeitico2.exibirFeitico()
+novoFeitico3.exibirFeitico()
+novoFeitico4.exibirFeitico()
+
+// Desafio: Poção e Ingredientes 
+
+export class Pocao{
+    private nome:string
+    private ingredientes:Array<object>
+    private efeito:string
+
+    setAtributos(nome:string, ingredientes:Array<object>, efeito:string):void{
+        this.nome = nome
+        this.ingredientes = ingredientes
+        this.efeito = efeito
+    }
+
+    getIngredientes():void{
+        console.log(this.ingredientes)
+    }
+
+    exibirPocao():void{
+        let ing1 = this.ingredientes
+        console.log(`A Poção do morto vivo
+Efeito: ${this.efeito}.
+Ingredientes: 
+
+`)
+    }
+}
+
+export class Ingrediente{
+    private nome:string
+    private quantidade:number
+    private efeito:string
+
+    setAtributos(nome:string, quantidade:number, efeito:string):void{
+        this.nome = nome,
+        this.quantidade = quantidade,
+        this.efeito = efeito
+    }
+}
+
+const ingrediente1 = new Ingrediente()
+ingrediente1.setAtributos('Vagem Soporífera', 1, 'Sono')
+const ingrediente2 = new Ingrediente()
+ingrediente2.setAtributos('Raíz de Afódelo em Pó', 1, 'Prolongamento de Sono')
+
+const pocao1 = new Pocao()
+pocao1.setAtributos('Poção do Morto-Vivo', [ingrediente1, ingrediente2], 'Faz a pessoa entrar em um sono profundo similar a morte.')
+
+pocao1.getIngredientes()
+pocao1.exibirPocao()
