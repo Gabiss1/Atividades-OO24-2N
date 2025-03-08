c058a56a8aeb685f164ef72023af737b6386d688
Gabiss1 <136657181+Gabiss1@users.noreply.github.com>
2025-03-07 21:55:29 -0300

    Add files via upload

--- src/06-03-25/Dinamica-JogosVorazes/tributos.ts
+++ src/06-03-25/Dinamica-JogosVorazes/tributos.ts
@@ -1,3 +1,5 @@
+import { Districts } from "./distrito";
+
 export class Monster{
     monsterName:string
     monsterHealth:number
@@ -53,8 +55,6 @@ export class Monster{
         return this.monsterStrength
     }
 }
-
-class Districts{}
 
 export class Skills{
     private skillName:string
@@ -168,46 +168,20 @@ export class Tributes{
         }
     }
 
-    collectResource
+    collectResource():void{
+        console.log(`O tributo ${this.name} encontrou ${this.district.getResources()}.`)
+    }
 }
 
-const district1 = new Districts()
+const distrito1 = new Districts()
+distrito1.setAtributes('Distrito 1',['Jóias'], 3125)
 
 const tribute1 = new Tributes()
-tribute1.setAtributes('Kennen', 120, district1, 'Nada')
-
-const tribute2 = new Tributes()
-tribute2.setAtributes('Katarina', 80, district1, 'Nada')
+tribute1.setAtributes('Kennen', 120, distrito1, 'Nada')
 
 const skill1 = new Skills()
-const skill2 = new Skills()
-const skill3 = new Skills()
-const skill4 = new Skills()
-const skill5 = new Skills()
-const skill6 = new Skills()
-const skill7 = new Skills()
-const skill8 = new Skills()
-const skill9 = new Skills()
-const skill10 = new Skills()
-
 skill1.setAtributes('Choque', 'Dispara um raio.', 'Paralisia', 25)
-skill2.setAtributes('Teleporte', 'Teletransporta para outro local.', 'Teletransporte', 0)
-skill3.setAtributes('Turbilhão Cortante', 'Cria um campo de energia que gera multiplos raios.', 'Paralisia', 110)
-skill4.setAtributes('Chuva Icathiana', 'Lança vários projéteis no alvo.', 'Nenhum', 30)
-skill5.setAtributes('Barragem Incendiária', 'Dispara um pulso de chamas.', 'Queimadura', 60)
-skill6.setAtributes('Refúgio da Ovelha', 'Prende o usuário e o alvo em uma arena onde o alvo não pode causar dano ao usuário.', 'Imunidade', 5)
-skill7.setAtributes('Garra Fantasma', 'Ataca o alvo com um corte invisível.', 'Nenhum', 20)
-skill8.setAtributes('Hidro-Canhão', 'Cria um grande jato de água', 'Nenhum', 85)
-skill9.setAtributes('Raio Solar', 'Carrega e dispara um raio de energia solar.', 'Queimadura', 100)
-skill10.setAtributes('Queimar', 'Queima o alvo.', 'Queimadura', 25)
 
-
-tribute1.setSkills(skill1)
-tribute1.setSkills(skill3)
-tribute2.setSkills(skill1)
-
-tribute1.getSkills()
-tribute1.getTribute()
-
+tribute1.collectResource()
 // tribute1.useSkill(0, tribute2)
 // tribute1.useSkill(0, tribute2)
