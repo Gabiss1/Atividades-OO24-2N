a757e66780991ad799a79f527feec505b548436d
Gabriel Oliveira <136657181+Gabiss1@users.noreply.github.com>
2025-05-29 19:40:11 -0300

    Add files via upload

---
+++ src/23-05-25/main.ts
@@ -0,0 +1,6 @@
+import { Pessoa } from "./pessoa";
+import { Relacionavel } from "./pessoa";
+import { ArvoreGenealogica } from "./arvoreGenealogica";
+
+const eu = new Pessoa("Gabriel", "Garcez de Oliveira", "18/04/2006", 'Masculino')
+
